9ae70ac5af54e944790a50e0b82160ce34774dc0
Your Name <you@example.com>
2017-09-21 18:48:30 +0530

    first commit

---
+++ sample.java
@@ -0,0 +1,12 @@
+public class sample {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+	for(int i=1;i<=10;i++) {
+      System.out.println("Hello World"+i); // prints Hello World
+	}
+   }
+}
